6ca387aa456e0ab739edee49833195086079033e
JRichm <jblackenmusic@gmail.com>
2023-02-17 11:57:26 -0700

    completed gitInfo

--- assessment-js-basics-1/gitInfo.js
+++ assessment-js-basics-1/gitInfo.js
@@ -10,6 +10,10 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM ONE");
+
+let gitDefinition = "Git is a piece of software that lets you save/store changes in your code to a local or cloud based repository and is used for version control over."
+console.log(gitDefinition);
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +22,10 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM TWO");
+
+let gitHubDefinition = "Github is a website that lets users upload/download git repositories that can be shared or accessed easily using URL directories.";
+console.log(gitHubDefinition);
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -26,6 +34,10 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM THREE");
+
+let gitInitDefinition = "'git init' is a git command that initializes an empty git repo.";
+console.log(gitInitDefinition);
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -34,7 +46,10 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM FOUR");
 
+let gitCloneDefinition = "'git clone' is a git command that is used to copy a git repo. This command takes in one argument: the URL or directory of the repo to be copied.";
+console.log(gitCloneDefinition);
 //////////////////PROBLEM 5////////////////////
 /*
     Create a variable called 'gitStatusDefinition'.  
@@ -42,6 +57,10 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM FIVE");
+
+let gitStatusDefinition = "'git status' is a git command that is used to show changes made to the project since last commit.";
+console.log(gitStatusDefinition);
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -53,6 +72,13 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM SIX");
+
+let gitAddDefinition = "'git add' is a git command that adds a file from the repo folder to be staged. This command takes in one argument: the file you want to be staged";
+let gitAddCode = "'git add .' adds all files from repo folder to be staged";
+
+console.log(gitAddDefinition);
+console.log(gitAddCode);
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -64,7 +90,13 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM SEVEN");
 
+let gitCommitDefinition = "'git commit' is a git command that applies changes that have been added to the staging area. You can use '-m message' to add a comment or message when using this command. Each commit is almost like a snapshot of your code, which can be referenced or reverted back to."
+let gitCommitCode = "'git commit -m initial commit'"
+
+console.log(gitCommitDefinition);
+console.log(gitCommitCode);
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +105,7 @@
 */
 
 //CODE HERE
+console.log("\nPROBELM EIGHT");
+
+let gitPushDefinition = "'git push' is a git command that uploads local repo data to your cloud based repo."
+console.log(gitPushDefinition);
